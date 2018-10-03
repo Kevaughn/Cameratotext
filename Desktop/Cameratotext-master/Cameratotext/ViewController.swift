4d4ec92e30b20f44a16bc3ad020991f15b30ea3f
Kevaughn <kevaughn@zacharys-air.resource.campus.njit.edu>
2018-10-03 11:55:32 -0400

    first commit

---
+++ Desktop/Cameratotext-master/Cameratotext/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Cameratotext
+//
+//  Created by Kevaughn on 23/10/2017.
+//  Copyright © 2017 Kevaughn. All rights reserved.
+//
+
+import UIKit
+import TesseractOCR
+
+class ViewController: UIViewController, G8TesseractDelegate {
+
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        if let tesseract = G8Tesseract(language: "eng") {
+            tesseract.delegate = self
+            tesseract.image = UIImage(named: "demoText")?.g8_blackAndWhite()
+            tesseract.recognize()
+            
+            textView.text = tesseract.recognizedText
+            
+        }
+        
+        
+    }
+    
+    func progressImageRecognition(for tesseract: G8Tesseract!) {
+        print("Recognition Progress \(tesseract.progress) %")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
